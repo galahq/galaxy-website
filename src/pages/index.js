ed6cb6d11a1dc1617c97bda518ed7e5a8309d0b0
Cameron Bothner <cameronbothner@gmail.com>
2018-01-26 01:17:36 -0500

    feat: use freight and tenso in a draft of running text styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,151 @@
+/**
+ * @providesModule HomePage
+ * @flow
+ */
+
 import React from 'react'
 import Link from 'gatsby-link'
 
-const HomePage = () => <div />
+import Button from '../components/Button'
+
+const HomePage = () => (
+  <div>
+    <h1>Gala (apple)</h1>
+    <p>
+      <b>Gala</b> is a{' '}
+      <a href="/wiki/Cloning#Horticultural" title="Cloning">
+        clonally propagated
+      </a>{' '}
+      <a href="/wiki/Apple" title="Apple">
+        apple
+      </a>{' '}
+      <a href="/wiki/Cultivar" title="Cultivar">
+        cultivar
+      </a>{' '}
+      with a mild and sweet flavor. <i>Gala</i> apples ranked at number 2 in
+      2006 on the{' '}
+      <a href="/wiki/US_Apple_Association" title="US Apple Association">
+        US Apple Association
+      </a>'s list of most popular apples, after{' '}
+      <i>
+        <a href="/wiki/Red_Delicious" title="Red Delicious">
+          Red Delicious
+        </a>
+      </i>{' '}
+      and before{' '}
+      <i>
+        <a href="/wiki/Golden_Delicious" title="Golden Delicious">
+          Golden Delicious
+        </a>
+      </i>,{' '}
+      <i>
+        <a href="/wiki/Granny_Smith" title="Granny Smith">
+          Granny Smith
+        </a>
+      </i>, and{' '}
+      <i>
+        <a href="/wiki/Fuji_(apple)" title="Fuji (apple)">
+          Fuji
+        </a>
+      </i>{' '}
+      (in order).<sup id="cite_ref-2" class="reference">
+        <a href="#cite_note-2">[2]</a>
+      </sup>{' '}
+      The skin color of the fruit is{' '}
+      <a
+        href="/wiki/Bicolored_apples"
+        class="mw-redirect"
+        title="Bicolored apples"
+      >
+        non-uniform
+      </a>.
+    </p>
+    <h2>
+      <span class="mw-headline" id="Appearance_and_flavor">
+        Appearance and flavor
+      </span>
+    </h2>
+    <p>
+      Gala apples are vertically striped or mottled, overall orange in colour.<sup
+        id="cite_ref-NFC_1-1"
+        class="reference"
+      >
+        <a href="#cite_note-NFC-1">[1]</a>
+      </sup>{' '}
+      They are sweet, fine textured, and aromatic,<sup
+        id="cite_ref-NFC_1-2"
+        class="reference"
+      >
+        <a href="#cite_note-NFC-1">[1]</a>
+      </sup>{' '}
+      can be added to salads, cooked, or eaten raw, and are especially suitable
+      for creating sauces.<sup id="cite_ref-3" class="reference">
+        <a href="#cite_note-3">[3]</a>
+      </sup>
+    </p>
+    <Button>Register</Button>
+    <h2>
+      <span class="mw-headline" id="History">
+        History
+      </span>
+    </h2>
+    <p>
+      The first Gala apple tree was one of many seedlings resulting from a cross
+      between a{' '}
+      <a href="/wiki/Golden_Delicious" title="Golden Delicious">
+        Golden Delicious
+      </a>{' '}
+      and a{' '}
+      <a href="/wiki/Kidd%27s_Orange_Red" title="Kidd's Orange Red">
+        Kidd's Orange Red
+      </a>{' '}
+      planted in{' '}
+      <a href="/wiki/New_Zealand" title="New Zealand">
+        New Zealand
+      </a>{' '}
+      in the 1930s by orchardist{' '}
+      <a
+        href="/w/index.php?title=J.H._Kidd&amp;action=edit&amp;redlink=1"
+        class="new"
+        title="J.H. Kidd (page does not exist)"
+      >
+        J.H. Kidd
+      </a>. Donald W. McKenzie, an employee of Stark Bros Nursery, obtained a US
+      plant patent for the cultivar on October 15, 1974.<sup
+        id="cite_ref-4"
+        class="reference"
+      >
+        <a href="#cite_note-4">[4]</a>
+      </sup>{' '}
+    </p>
+    <p>
+      It is a relatively new introduction to the UK, first planted in commercial
+      volumes during the 1980s. The variety now represents about 20% of the
+      total volume of the commercial production of eating apples grown in the
+      UK, often replacing{' '}
+      <a href="/wiki/Cox%27s_Orange_Pippin" title="Cox's Orange Pippin">
+        Cox's Orange Pippin
+      </a>.
+    </p>
+    <h3>
+      <span id="Sports_.28mutations.29" />
+      <span class="mw-headline" id="Sports_(mutations)">
+        Sports (mutations)
+      </span>
+    </h3>
+    <p>
+      Many{' '}
+      <a href="/wiki/Sport_(botany)" title="Sport (botany)">
+        sports
+      </a>{' '}
+      of Gala have been selected, mostly for increased red color, including the
+      popular <i>Royal Gala</i>. The original cultivar produced fruit with
+      orange stripes and a partial orange blush over a yellow background. Since
+      then, several un-patented sports have been recognized. Additionally, more
+      than twenty sports have received US plant patents:
+    </p>{' '}
+    <Button>Register</Button>
+  </div>
+)
 
 export default HomePage
