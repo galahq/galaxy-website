6b0499f08a625cdf3c92c7fd2ebaebd9bc77e798
Cameron Bothner <cameronbothner@gmail.com>
2018-02-08 09:59:50 -0500

    feat: add an overview to the studio pages

--- src/pages/studio.js
+++ src/pages/studio.js
@@ -6,12 +6,24 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
+import StreamOverview from '../components/streams/Overview'
 import StreamNavigation from '../components/streams/Navigation'
 import MaxWidthContainer from '../components/shared/MaxWidthContainer'
+
+const StudiosIcon: any = require('../assets/studios.svg')
 
 const StudioPage = () => (
   <main>
     <StreamNavigation active="studio" />
+    <StreamOverview
+      title="Case Design Studios"
+      tagline="Do a cool thing with people"
+      pitch="People interested in learning about how to produce a case study,
+      or produce one better, will come with a case subject and materials attend
+      a series of hands-on sessions, and leave with a draft of a case, and
+      visualize its presentation on Gala."
+      icon={<StudiosIcon />}
+    />
 
     <MaxWidthContainer />
   </main>
