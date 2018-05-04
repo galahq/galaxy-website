4358ab9df679c54876fe2bf083e2a542552ae7da
Cameron Bothner <cameronbothner@gmail.com>
2018-05-04 11:14:04 -0400

    fix: update logistics page

--- src/pages/logistics.js
+++ src/pages/logistics.js
@@ -10,9 +10,7 @@ import TitleSection from '../components/shared/TitleSection'
 import MaxWidthContainer, {
   UnpaddedRunningTextContainer,
   RunningTextContainer,
-  PaddedRunningTextContainer,
 } from '../components/shared/MaxWidthContainer'
-import PriceTable from '../components/PriceTable'
 import { ExternalLink, ExternalButton } from '../components/shared/Link'
 import Card from '../components/shared/Card'
 
@@ -20,83 +18,37 @@ const LogisticsPage = () => (
   <main>
     <TitleSection>
       <RunningTextContainer>
-        <h1>Logistics</h1>
+        <h1>Logistics &amp; Accommodations</h1>
       </RunningTextContainer>
     </TitleSection>
 
     <RunningTextContainer>
-      <h2>Accomodations</h2>
+      <h2>Lodging</h2>
       <p>
-        We suggest three local hotels and B&Bs that we often support and who
-        support us. We recommend them as they are walkable to our events.
+        We suggest these local hotels and B&amp;B’s that are walkable to our
+        events and offer a group rate for Galaxy participants.
       </p>
     </RunningTextContainer>
 
     <MaxWidthContainer>
       <Row>
         <LinksContainer>
-          <BareExternalLink href="https://www.stonechalet.com/">
-            <Card>
-              <h3>The Stone Chalet</h3>
-              <Details>
-                Located in a quiet residential neighborhood near the University
-                of Michigan’s Ann Arbor Central Campus.
-              </Details>
-              <Details>Group rate not available. </Details>
-            </Card>
-          </BareExternalLink>
-          <Location href="https://www.google.com/maps/dir/Stone+Chalet+Bed+and+Breakfast+Inn+and+Event+Center,+1917+Washtenaw+Ave,+Ann+Arbor,+MI+48104/Rackham+Auditorium,+915+E+Washington+St,+Ann+Arbor,+MI+48109/@42.2744971,-83.7346294,16z/data=!3m1!4b1!4m14!4m13!1m5!1m1!1s0x883cae56f8223a45:0x33ae5a4e1bcf5b26!2m2!1d-83.722327!2d42.2684559!1m5!1m1!1s0x883cae41b17e2f4d:0xd7199f2dad45bc94!2m2!1d-83.7381556!2d42.2807892!3e2">
-            1 mi
-          </Location>
+          <Card>
+            <h3>Bell Tower Hotel</h3>
+            <Location href="https://www.google.com/maps/dir/Bell+Tower+Hotel,+300+S+Thayer+St,+Ann+Arbor,+MI+48104/Rackham+Auditorium,+915+E+Washington+St,+Ann+Arbor,+MI+48109/@42.2800158,-83.7411187,17z/data=!3m1!4b1!4m14!4m13!1m5!1m1!1s0x883cae40fddeb7b9:0x597a9dcf3d549001!2m2!1d-83.739903!2d42.2792424!1m5!1m1!1s0x883cae41b17e2f4d:0xd7199f2dad45bc94!2m2!1d-83.7381556!2d42.2807892!3e2">
+              &lt; 1 mi
+            </Location>
+            <p>$199 + taxes per night.</p>
+            <Details>
+              A charming hotel located in the heart of downtown Ann Arbor on the
+              University of Michigan’s main campus.
+            </Details>
+            <Details>Use code 331374 when booking online.</Details>
+            <Button href="https://www.belltowerhotel.com/">Book now</Button>
+          </Card>
         </LinksContainer>
 
-        <LinksContainer>
-          <BareExternalLink href="https://www.belltowerhotel.com/">
-            <Card>
-              <h3>Burnt Toast Inn</h3>
-              <Details>
-                A small, family owned and operated bed and breakfast established
-                in 2004 in downtown Ann Arbor.
-              </Details>
-              <Details>Group rate not available.</Details>   
-            </Card>
-          </BareExternalLink>
-          <Location href="https://www.google.com/maps/dir/Burnt+Toast+Inn,+415+W+William+St,+Ann+Arbor,+MI+48103/Rackham+Auditorium,+915+E+Washington+St,+Ann+Arbor,+MI+48109/@42.2792995,-83.7498665,16z/data=!3m1!4b1!4m14!4m13!1m5!1m1!1s0x883cae23586e337f:0x7096d929cf547999!2m2!1d-83.752998!2d42.27781!1m5!1m1!1s0x883cae41b17e2f4d:0xd7199f2dad45bc94!2m2!1d-83.7381556!2d42.2807892!3e2">
-            1 mi
-          </Location>
-        </LinksContainer>
-
-        <LinksContainer>
-          <BareExternalLink href="https://www.belltowerhotel.com/">
-            <Card>
-              <h3>Bell Tower Hotel</h3>
-              <Details>         
-                A charming hotel located in the heart of downtown Ann Arbor on
-                the University of Michigan’s main campus.
-              </Details>
-              <Details>Group rate: $199 + taxes per night, breakfast included.</Details> 
-              <Details>Use code 331374 when booking online.</Details>
-            </Card>
-          </BareExternalLink>
-          <Location href="https://www.google.com/maps/dir/Bell+Tower+Hotel,+300+S+Thayer+St,+Ann+Arbor,+MI+48104/Rackham+Auditorium,+915+E+Washington+St,+Ann+Arbor,+MI+48109/@42.2800158,-83.7411187,17z/data=!3m1!4b1!4m14!4m13!1m5!1m1!1s0x883cae40fddeb7b9:0x597a9dcf3d549001!2m2!1d-83.739903!2d42.2792424!1m5!1m1!1s0x883cae41b17e2f4d:0xd7199f2dad45bc94!2m2!1d-83.7381556!2d42.2807892!3e2">
-            0 mi
-          </Location>
-        </LinksContainer>
-      </Row>
-    </MaxWidthContainer>
-    <RunningTextContainer>
-      <p>
-        In addition, Galaxy has room blocks at three branded hotels offering
-        discounted rates for participants. We will arrange with the hotels ahead
-        of time to have shuttles for Galaxy events, and you can schedule extra
-        complimentary shuttles with the hotel concierge. The Holiday Inn also
-        has an Avis/Budget car rental service, and all offer free parking.
-      </p>
-    </RunningTextContainer>
-
-    <MaxWidthContainer>
-      <Row>
-        <Card minimal>
+        <Card>
           <h3>Hyatt Place</h3>
           <Location href="https://www.google.com/maps/dir/Hyatt+Place+Ann+Arbor,+South+State+Street,+Ann+Arbor,+MI/Rackham+Auditorium,+East+Washington+Street,+Ann+Arbor,+MI/@42.2594116,-83.7960496,13z/data=!3m1!4b1!4m14!4m13!1m5!1m1!1s0x883caf95ac8c8abd:0xa3a133323db08ad3!2m2!1d-83.7381025!2d42.2404887!1m5!1m1!1s0x883cae41b17e2f4d:0xd7199f2dad45bc94!2m2!1d-83.7381556!2d42.2807892!3e0">
             3 mi
@@ -109,50 +61,8 @@ const LogisticsPage = () => (
             Or call (734) 995-1234 and mention “University of Michigan Galaxy
             Block”
           </Details>
-          <Details>Reserve by May 6, 2018 for the group rate.</Details>
-          <Button href="https://annarbor.place.hyatt.com/en/hotel/home.html?corp_id=G-GALA">
-            Book now
-          </Button>
-        </Card>
-
-        <Card minimal>
-          <h3>Holiday Inn</h3>
-          <Location href="https://www.google.com/maps/dir/Holiday+Inn+Near+the+University+of+Michigan,+3600+Plymouth+Rd,+Ann+Arbor,+MI+48105/Rackham+Auditorium,+East+Washington+Street,+Ann+Arbor,+MI/@42.3022153,-83.7515064,13z/data=!3m1!4b1!4m14!4m13!1m5!1m1!1s0x883cac1b75a197f1:0xbc6750e4373aaf8d!2m2!1d-83.689737!2d42.304177!1m5!1m1!1s0x883cae41b17e2f4d:0xd7199f2dad45bc94!2m2!1d-83.7381556!2d42.2807892!3e0">
-            4 mi
-          </Location>
-          <p>
-            $114 + taxes per night.<br />
-          </p>
-          <Details>
-            Enter dates and you will automatically be given the special rate.
-          </Details>
-          <Details>
-            Or call (734) 769-9800 and mention “University of Michigan Galaxy
-            Block.”
-          </Details>
           <Details>Reserve by May 23, 2018 for the group rate.</Details>
-          <Button href="http://ichotelsgroup.com/redirect?path=rates&brandCode=HI&GPC=GR6&hotelCode=ARBNC&_PMID=99801505">
-            Book now
-          </Button>
-        </Card>
-
-        <Card minimal>
-          <h3>Kensington Hotel</h3>
-          <Location href="https://www.google.com/maps/dir/The+Kensington+Hotel,+South+State+Street,+Ann+Arbor,+MI/Rackham+Auditorium,+East+Washington+Street,+Ann+Arbor,+MI/@42.2587246,-83.7960496,13z/data=!4m14!4m13!1m5!1m1!1s0x883cafeb34b26bed:0xe2cbfb9a5e565358!2m2!1d-83.7405497!2d42.2389335!1m5!1m1!1s0x883cae41b17e2f4d:0xd7199f2dad45bc94!2m2!1d-83.7381556!2d42.2807892!3e0">
-            4 mi
-          </Location>
-          <p>
-            $114 + taxes per night.<br />
-          </p>
-          <Details>Use group code “Galaxy” to book online.</Details>
-          <Details>
-            Or call (734) 761-7800 and mention “Galaxy room block.”
-          </Details>
-          <Details>Reserve by May 6, 2018 for the group rate.</Details>
-          <Details>
-            <strong>Note:</strong> This hotel does not have a shuttle service.
-          </Details>
-          <Button href="https://gc.synxis.com/rez.aspx?Hotel=69682&Chain=19265&arrive=6/6/2018&depart=6/9/2018&adult=1&child=0&group=Galaxy">
+          <Button href="https://annarbor.place.hyatt.com/en/hotel/home.html?corp_id=G-GALA">
             Book now
           </Button>
         </Card>
@@ -209,19 +119,6 @@ const LinksContainer = styled.div`
   color: black;
 `
 
-const BareExternalLink = ExternalLink.extend`
-  text-decoration: none;
-
-  &:hover .o-card {
-    box-shadow: 0 0 0 1px rgba(16, 22, 26, 0.2), 0 2px 4px rgba(16, 22, 26, 0.4),
-      0 8px 24px rgba(16, 22, 26, 0.4);
-  }
-
-  & a {
-    text-decoration: underline;
-  }
-`
-
 const Button = ExternalButton.extend`
   margin-top: auto;
 `
@@ -229,8 +126,9 @@ const Button = ExternalButton.extend`
 const Row = styled.div`
   display: grid;
   grid-gap: 1.5em;
-  grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
-  margin-bottom: 2rem;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  margin: 0 auto 2rem;
+  max-width: 700px;
 `
 
 const Details = styled.p`
