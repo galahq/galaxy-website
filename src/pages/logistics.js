8519c7ef0bef75d2da9c0a360db7d2305360fa16
Pearl Zeng <zhuzeng@umich.edu>
2019-02-15 18:03:40 -0500

    update: delete last year's hotel info

--- src/pages/logistics.js
+++ src/pages/logistics.js
@@ -18,56 +18,9 @@ const LogisticsPage = () => (
   <main>
     <TitleSection>
       <RunningTextContainer>
-        <h1>Logistics &amp; Accommodations</h1>
+        <h1>Location</h1>
       </RunningTextContainer>
     </TitleSection>
-
-    <RunningTextContainer>
-      <h2>Lodging</h2>
-      <p>
-        We suggest these local hotels and B&amp;B’s that are walkable to our
-        events and offer a group rate for Galaxy participants.
-      </p>
-    </RunningTextContainer>
-
-    <MaxWidthContainer>
-      <Row>
-        <LinksContainer>
-          <Card>
-            <h3>Bell Tower Hotel</h3>
-            <Location href="https://www.google.com/maps/dir/Bell+Tower+Hotel,+300+S+Thayer+St,+Ann+Arbor,+MI+48104/Rackham+Auditorium,+915+E+Washington+St,+Ann+Arbor,+MI+48109/@42.2800158,-83.7411187,17z/data=!3m1!4b1!4m14!4m13!1m5!1m1!1s0x883cae40fddeb7b9:0x597a9dcf3d549001!2m2!1d-83.739903!2d42.2792424!1m5!1m1!1s0x883cae41b17e2f4d:0xd7199f2dad45bc94!2m2!1d-83.7381556!2d42.2807892!3e2">
-              &lt; 1 mi
-            </Location>
-            <p>$199 + taxes per night.</p>
-            <Details>
-              A charming hotel located in the heart of downtown Ann Arbor on the
-              University of Michigan’s main campus.
-            </Details>
-            <Details>Use code 331374 when booking online.</Details>
-            <Button href="https://www.belltowerhotel.com/">Book now</Button>
-          </Card>
-        </LinksContainer>
-
-        <Card>
-          <h3>Hyatt Place</h3>
-          <Location href="https://www.google.com/maps/dir/Hyatt+Place+Ann+Arbor,+South+State+Street,+Ann+Arbor,+MI/Rackham+Auditorium,+East+Washington+Street,+Ann+Arbor,+MI/@42.2594116,-83.7960496,13z/data=!3m1!4b1!4m14!4m13!1m5!1m1!1s0x883caf95ac8c8abd:0xa3a133323db08ad3!2m2!1d-83.7381025!2d42.2404887!1m5!1m1!1s0x883cae41b17e2f4d:0xd7199f2dad45bc94!2m2!1d-83.7381556!2d42.2807892!3e0">
-            3 mi
-          </Location>
-          <p>
-            $114 + taxes per night.<br />
-          </p>
-          <Details>Use code G-GALA when booking online.</Details>
-          <Details>
-            Or call (734) 995-1234 and mention “University of Michigan Galaxy
-            Block”
-          </Details>
-          <Details>Reserve by May 23, 2018 for the group rate.</Details>
-          <Button href="https://annarbor.place.hyatt.com/en/hotel/home.html?corp_id=G-GALA">
-            Book now
-          </Button>
-        </Card>
-      </Row>
-    </MaxWidthContainer>
 
     <UnpaddedRunningTextContainer>
       <h2>Location</h2>
@@ -78,11 +31,9 @@ const LogisticsPage = () => (
         Michigan Central Campus in Downtown Ann Arbor.
       </p>
       <p>
-        All are also within walking distance of two open-air festival events
-        which run during Galaxy—the Mayor’s Green Fair, which will close Liberty
-        and Main Streets, and the Ann Arbor Summer Festival, which closes
-        Washington Street—for pedestrian flow between environmental and cultural
-        events.
+        All are also within walking distance of the open-air Ann Arbor Summer
+        Festival, which closes Washington Street—for pedestrian flow between
+        environmental and cultural events.
       </p>
     </UnpaddedRunningTextContainer>
     <Map>
