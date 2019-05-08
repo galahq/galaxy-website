8a21a4754ec5dcdc637baef28a8ac344ce7059e5
Cameron Bothner <cameronbothner@gmail.com>
2019-05-08 15:04:42 -0400

    Add hotel information

--- src/pages/logistics.js
+++ src/pages/logistics.js
@@ -9,7 +9,7 @@ import styled from 'styled-components'
 import TitleSection from '../components/shared/TitleSection'
 import MaxWidthContainer, {
   UnpaddedRunningTextContainer,
-  RunningTextContainer,
+  RunningTextContainer
 } from '../components/shared/MaxWidthContainer'
 import { ExternalLink, ExternalButton } from '../components/shared/Link'
 import Card from '../components/shared/Card'
@@ -42,6 +42,45 @@ const LogisticsPage = () => (
         src="//www.google.com/maps/d/u/0/embed?mid=1x2cJRHxrZovA_eFdHe8uKH_ThZnQBrrO"
       />
     </Map>
+
+    <RunningTextContainer>
+      <h2>Lodging</h2>
+      <p>
+        We suggest these local hotels that offer a group rate for Galaxy
+        participants.
+      </p>
+    </RunningTextContainer>
+
+    <MaxWidthContainer>
+      <Row>
+        <LinksContainer>
+          <Card>
+            <h3>Sonesta ES Suites</h3>
+            <Location href="https://www.google.com/maps/dir/Sonesta+ES+Suites+Ann+Arbor,+Victors+Way,+Ann+Arbor,+MI/Rackham+Auditorium,+East+Washington+Street,+Ann+Arbor,+MI/@42.2595678,-83.7486003,14z/data=!3m1!4b1!4m14!4m13!1m5!1m1!1s0x883caf9430df260f:0x8c26489124de3d66!2m2!1d-83.7359546!2d42.2384454!1m5!1m1!1s0x883cae41b17e2f4d:0xd7199f2dad45bc94!2m2!1d-83.7381556!2d42.2807892!3e0">
+              3 mi
+            </Location>
+            <p>$119 + taxes per night.</p>
+            <Details>Book before May 29 for the discounted rate.</Details>
+            <Details>Two-bedroom suites also available.</Details>
+            <Button href="https://gc.synxis.com/rez.aspx?Hotel=77468&Chain=5157&arrive=6/19/2019&depart=6/22/2019&adult=1&child=0&group=06190619GA">
+              Book now
+            </Button>
+          </Card>
+        </LinksContainer>
+
+        <Card>
+          <h3>Holiday Inn</h3>
+          <Location href="https://www.google.com/maps/dir/Holiday+Inn+Near+the+University+of+Michigan,+Plymouth+Road,+Ann+Arbor,+MI/Rackham+Auditorium,+East+Washington+Street,+Ann+Arbor,+MI/@42.3022194,-83.7339109,14z/data=!3m1!4b1!4m14!4m13!1m5!1m1!1s0x883cac1b75a197f1:0xbc6750e4373aaf8d!2m2!1d-83.689737!2d42.304177!1m5!1m1!1s0x883cae41b17e2f4d:0xd7199f2dad45bc94!2m2!1d-83.7381556!2d42.2807892!3e0">
+            4 mi
+          </Location>
+          <p>$124 + taxes per night.</p>
+          <Details>Book before May 29 for the discounted rate.</Details>
+          <Button href="https://www.holidayinn.com/redirect?path=rates&brandCode=HI&localeCode=en&regionCode=1&hotelCode=ARBNC&checkInDate=19&checkInMonthYear=052019&checkOutDate=22&checkOutMonthYear=052019&_PMID=99801505&GPC=GYC&cn=no&viewfullsite=true">
+            Book now
+          </Button>
+        </Card>
+      </Row>
+    </MaxWidthContainer>
 
     <RunningTextContainer>
       <h2>Transportation</h2>
